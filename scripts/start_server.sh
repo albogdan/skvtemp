510031eaf9f49237f7f7b31b5102e0847e6a636d
AB <stefanalexb@gmail.com>
2021-06-18 18:04:35 -0400

    application run as root?

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/skule-vote/skule_vote
-gunicorn skule_vote.wsgi:application --bind 0.0.0.0:80 --workers 5 --capture-output --access-logfile - --error-logfile -
+sudo gunicorn skule_vote.wsgi:application --bind 0.0.0.0:80 --workers 5 --capture-output --access-logfile - --error-logfile -
