8924e87992825dcc86735bed00a55b9a9db0a82c
AB <stefanalexb@gmail.com>
2021-08-02 14:35:33 -0400

    Added docker deployment scripts v1

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd /home/ec2-user/skule-vote/skule_vote
-sudo gunicorn skule_vote.wsgi:application --bind 0.0.0.0:80 --workers 5 --capture-output --access-logfile - --error-logfile -
+docker-compose -f deployment/docker-compose.prod.yml up -d
