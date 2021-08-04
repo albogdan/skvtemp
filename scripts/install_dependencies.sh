b6ba8a6029701815a2521ef1379404995c183c4e
AB <stefanalexb@gmail.com>
2021-08-04 17:50:41 -0400

    minor change to test 4 debug

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,7 +3,9 @@ cd /home/ec2-user/skule-vote/skule_vote
 docker-compose -f deployment/docker-compose.prod.yml down
 sudo rm -r /var/www/html/*
 
-sudo pip3 install -r requirements.txt
+virtualenv venv
+source venv/bin/activate
+sudo pip install -r requirements.txt
 
 cd frontend/ui
 yarn in && yarn build
