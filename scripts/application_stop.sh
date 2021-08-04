7f5d21065fb97621dcbf7c637c7d9c2f7c81d8c7
AB <stefanalexb@gmail.com>
2021-08-04 18:25:55 -0400

    env vars pt 2

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/ec2-user/skule-vote/skule_vote
+cd /home/ec2-user/skule-vote
 docker-compose -f deployment/docker-compose.prod.yml down
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,3 @@
 cp /ENV_VARS /home/ec2-user/skule-vote/deployment
 docker-compose -f deployment/docker-compose.prod.yml build
 docker-compose -f deployment/docker-compose.prod.yml up -d
-rm /home/ec2-user/skule-vote/deployment/ENV_VARS
