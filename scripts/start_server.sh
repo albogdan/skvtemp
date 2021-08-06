490c292280d8f17b839d975f3a130c757d4454ec
AB <stefanalexb@gmail.com>
2021-08-06 15:10:32 -0400

    env vars pt 4

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd /home/ec2-user/skule-vote
 cp /ENV_VARS /home/ec2-user/skule-vote/deployment
 docker-compose -f deployment/docker-compose.prod.yml build
 docker-compose -f deployment/docker-compose.prod.yml up -d
