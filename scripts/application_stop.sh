4698ae26ec95ad5a88b8cdf3f29a18eceb79723c
AB <stefanalexb@gmail.com>
2021-08-06 15:22:43 -0400

    check if skule-vote exists when bringing down

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-cd /home/ec2-user/skule-vote
-docker-compose -f deployment/docker-compose.prod.yml down
+
+FOLDER=/home/ec2-user/skule-vote
+if [ -e "$FOLDER" ]; then
+  cd $FOLDER
+  docker-compose -f deployment/docker-compose.prod.yml down
+else
+  echo "$FOLDER does not exist."
+fi
